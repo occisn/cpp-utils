b51e141b355217f09d63a7116a8f5522ac25554b
Nicolas O <noccis@I7413>
2025-12-25 12:43:30 +0100

    Improve function to measure execution duration

--- src/duration.cpp
+++ src/duration.cpp
@@ -3,9 +3,9 @@
 
 int measure_duration(void)
 {
-  auto start = std::chrono::high_resolution_clock::now();
+  auto start = std::chrono::steady_clock::now();
   // do something
-  auto end = std::chrono::high_resolution_clock::now();
+  auto end = std::chrono::steady_clock::now();
   std::chrono::duration<double> duration = end - start;
   std::cout << "Duration: " << duration.count() << " seconds" << std::endl;
   return 0;
